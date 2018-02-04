9e18a51ec02bc51ef4d52c4802f393cf43ce6cfc
shun <shun@fuji.local>
2018-02-05 01:48:08 +0900

    1分たつとエラーになることが判明、viewDidLoad内のUIMenuControllerが元のコードと大きく違うため削除、全て正常に動作

--- sleepMeister/ViewController.swift
+++ sleepMeister/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         label.text = getNowTime()
-        _ = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
+        _ = Timer.scheduledTimer(timeInterval: 60, target: self, selector: #selector(update), userInfo: nil, repeats: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,35 +29,37 @@ class ViewController: UIViewController {
     private var setTime : String = "00:00"
     
     @IBAction func DPFunc(_ sender: Any) {
+        print("test: DP moved!")
         let format = DateFormatter()
         format.dateFormat = "HH:mm"
         tempTime = format.string(from: DP.date)
         
     }
     @IBAction func alarmSetBtn(_ sender: Any) {
+        print("test: btn moved!")
         setTime = tempTime
     }
     
     func getNowTime()->String{
         let nowTime: NSDate = NSDate()
         let format = DateFormatter()
-        format.dateFormat = "HH:mm"
+            format.dateFormat = "HH:mm"
         let nowTimeStr = format.string(from: nowTime as Date)
         return nowTimeStr
     }
     
-    func update(){
+    @objc func update(){
         let str = getNowTime()
         label.text = str
         myAlarm(str: str)
     }
     
-    func myAlarm(str: String){
+     func myAlarm(str: String){
         func alert(){
             let myAlert = UIAlertController(title: "alert", message:"ring ding", preferredStyle: .alert)
             let myAction = UIAlertAction(title: "止める", style: .default){
                 action in print("foo!")
-            }
+                }
             myAlert.addAction(myAction)
             present(myAlert, animated: true, completion: nil)
         };        if str == setTime{
